ae0d9fed43ffd5ad9fb465807f92a199307353d4
Aly Smith <alimay.smith@outlook.com>
2022-07-28 14:33:15 -0600

    adding basic files for css and images

--- web/index.php
+++ web/index.php
@@ -4,9 +4,20 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/web/css/main.css">
+  <link rel="stylesheet" href="/web/css/layout.css">
   <title>University of Alderaan</title>
 </head>
 <body>
   <h1>Welcome to the University of Alderaan</h1>
+  <img src="/web/img/rua-logo.png" alt="University of Alderaan Logo">
+  <p>This site is currently under a lot of construction</p>
+  <?php 
+    $d=mktime(0, 0, 0, 7, 28, 2022);
+    echo "Last Updated on " . date("m-d-Y", $d);
+  ?>
+  <footer>
+    &copy; aVanos - <?php echo date("M Y");?>
+  </footer>
 </body>
 </html>
